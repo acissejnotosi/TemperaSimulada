428ca9de200b8d29b2da030cf3168a34bf18a9aa
Jacichen <Jacichen@192.168.100.55>
2016-04-27 22:13:43 -0300

    Metodo getFitness finalizado

--- SimulatedAnnealing/src/simulatedannealing/FitnessFunction.java
+++ SimulatedAnnealing/src/simulatedannealing/FitnessFunction.java
@@ -46,30 +46,36 @@ public class FitnessFunction {
     
     private int countErrors(boolean state[], List<Integer> intervals){
         int errors = 0;
+        //Os algoritmos devem considerar que intervals esta ordenado para funcionar
         errors += countNotIncludedObrigatoryDisciplines(state);
         errors += countRepeatedDisciplines(state);
         errors += countRepeatedIntervals(intervals);
-        errors += countFiveIntervalsSequence(intervals);
-        errors += countTwelveIntervalNotBreak(intervals);
+        errors += countMoreThanFiveIntervalsSequence(intervals);
+        errors += countLessThanTwelveIntervalBetweenDays(intervals);
         return errors;
     }
     
     private int countNotIncludedObrigatoryDisciplines(boolean state[]){
         Discipline[] obrigatories = getObrigatories();
         List<String> tokensNotTaken = new ArrayList<>();
+        //Para cada disciplina obrigatória existente crie um marcacador de que ele deve ser retirado
         for(Discipline d: obrigatories)
             if(!tokensNotTaken.contains(d))
                 tokensNotTaken.add(d.getName().toUpperCase());
+        //Para cada disciplina selecionada em state, remove um marcador se ela for obrigatoria
         for(int i = 0; i < state.length; i++)
             if(state[i])
                 if(tokensNotTaken.contains(discips[i].getName().toUpperCase()))
                     tokensNotTaken.remove(discips[i].getName().toUpperCase());
+        //Retorna o número de marcadores que sobraram
         return tokensNotTaken.size();
     }
 
     private int countRepeatedDisciplines(boolean state[]){
         int countRep = 0;
         List<String> repeatedTokens =  new ArrayList<>();
+        //Se não existir uma disciplina selecionada entre os marcadores, insira um...
+        //Caso exista, então foi encontrado uma disciplina repetida e deve-se aumentar o contador 
         for(int i = 0; i < state.length; i++){
             if(state[i]){
                 if(repeatedTokens.contains(discips[i].getDisciplineName().toUpperCase()))
@@ -85,6 +91,8 @@ public class FitnessFunction {
     private int countRepeatedIntervals(List<Integer> intervals){
         int countRep = 0;
         List<Integer> repeatedTokens = new ArrayList<>();
+        //Se não existir o marcador do intervalo em questão, então insira um...
+        //Caso exista, então foi encontrado um disciplina com horário sobreposto e deve-se aumentar o contador 
         for(Integer i: intervals)
             if(repeatedTokens.contains(i))
                 countRep++;
@@ -93,12 +101,38 @@ public class FitnessFunction {
         return countRep++;
     }
     
-    private int countFiveIntervalsSequence(List<Integer> intervals){
-        return 0;
+    private int countMoreThanFiveIntervalsSequence(List<Integer> intervals){
+        int ini = 0,prox = 0, overflow = 0;
+        
+        //Verifica sequência de horarios para identificar se existem mais de 5 disciplinas em sequencia
+        for(;prox < intervals.size(); prox++){
+            //Se a diferença entre um elemento e o próximo é maior que um, então não estão mais em sequencia
+            if(intervals.get(prox)-intervals.get(prox-1) > 1){
+                //Se a sequencia for maior que 5, então aumente a contagem
+                if(prox - ini > 5)
+                    //Quanto mais disciplinas seguidas após 5 pior
+                    overflow += prox - ini - 5;                 
+                //Reseta inicio da sequencia
+                ini = prox;
+            }
+        }
+        
+        //Verificação de borda- último caso não se encaixa nas condições anteriores
+        prox--;
+        if(prox - ini > 5)
+            overflow += prox - ini - 4;
+               
+        return overflow;
     }
     
-    private int countTwelveIntervalNotBreak(List<Integer> intervals){
-        return 0;
+    private int countLessThanTwelveIntervalBetweenDays(List<Integer> intervals){
+        int contSmallDelay = 0;
+        for(int i = 1; i < intervals.size(); i++)
+            //Caso o intervalo entre a ultima aula de um dia e a primeira do dia seguinte seja menor que 
+            //12 horas deve incrementar o contador
+            if(intervals.get(i)-intervals.get(i-1) < 12 && intervals.get(i)/24 != intervals.get(i-1)/24)
+                contSmallDelay++;
+        return contSmallDelay;
     }
     
     private Discipline[] getObrigatories(){
